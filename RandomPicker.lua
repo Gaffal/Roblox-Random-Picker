e62bf5125e7706f12f9d807c5e35e127aeb109dd
Gaffal <58052106+Gaffal@users.noreply.github.com>
2021-06-21 10:58:45 -0300

    Add remove and destroy methods
    The module is a bit faster now.

--- RandomPicker.lua
+++ RandomPicker.lua
@@ -1,17 +1,41 @@
 local RandomPicker = {}
 RandomPicker.__index = RandomPicker
 
+local random = Random.new()
+
 function RandomPicker.new(items: Array | nil)
 	return setmetatable({
-		Items = items or {}
+		Items = items or {},
+		_total_sum = 0,
 	}, RandomPicker)
 end
 
 function RandomPicker:Add(item, rarity: number)
+	assert(not self.Destroyed, 'Picker is destroyed')
+
 	table.insert(self.Items, {
 		Item = item,
 		Rarity = rarity,
 	})
+
+	self._total_sum += rarity
+	self:_SortByRarity()
+
+	return self
+end
+
+function RandomPicker:Remove(item)
+	assert(not self.Destroyed, 'Picker is destroyed')
+
+	for index, item_data in ipairs(self.Items) do
+		if (item_data.Item == item) then
+			table.remove(self.Items, index)
+			self._total_sum -= item_data.Rarity
+		end
+	end
+
+	self:_SortByRarity()
+	return self
 end
 
 function RandomPicker:_GetTotalSum()
@@ -31,22 +55,33 @@ function RandomPicker:_SortByRarity()
 end
 
 function RandomPicker:Pick(seed: number | nil)
-	assert(#self.Items > 0)  -- Item list is empty
-	self:_SortByRarity()
+	assert(#self.Items > 0, 'No items to choose from.')
+	assert(not self.Destroyed, 'Picker is destroyed')
 
-	local totalSum = self:_GetTotalSum()
-	local random = ( seed and Random.new(seed) ) or Random.new()
-	local chance = random:NextInteger(1, totalSum)
+	local rnd = ( seed and Random.new(seed) ) or random
+	local chance = rnd:NextInteger(1, self._total_sum)
 
-	for _, item in ipairs(self.Items) do
-		if item.Rarity >= chance then
-			return item.Item
+	for _, item_data in ipairs(self.Items) do
+		if item_data.Rarity >= chance then
+			return item_data.Item
 		else
-			chance -= item.Rarity
+			chance -= item_data.Rarity
 		end
 	end
 
-	return self:Pick(seed)
+	return self:Pick()
+end
+
+function RandomPicker:SinglePick(seed: number | nil)
+	local item = self:Pick(seed)
+	self:Destroy()
+
+	return item
+end
+
+function RandomPicker:Destroy()
+	self.Items = nil
+	self.Destroyed = true
 end
 
 return RandomPicker
